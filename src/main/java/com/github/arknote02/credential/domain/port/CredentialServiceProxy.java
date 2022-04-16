190218e558e54ce0a63506838dede919603bd67a
Queue <kentaro.kiuchi@outlook.jp>
2022-04-16 21:47:03 +0900

    Rename module

--- src/main/java/com/github/arknote02/credential/domain/port/CredentialServiceProxy.java
+++ src/main/java/com/github/arknote02/credential/domain/port/CredentialServiceProxy.java
@@ -3,7 +3,7 @@ package com.github.arknote02.credential.domain.port;
 import com.github.arknote02.credential.domain.model.AccessToken;
 import com.github.arknote02.credential.domain.model.Credential;
 
-public interface AuthenticateServiceProxy {
+public interface CredentialServiceProxy {
 
   AccessToken authenticate(Credential credential);
 }
