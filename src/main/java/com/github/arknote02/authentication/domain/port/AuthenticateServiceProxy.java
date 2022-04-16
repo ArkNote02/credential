5dc867feeaa2345ec3a6eff396e09be25d30f0a0
Queue <kentaro.kiuchi@outlook.jp>
2022-04-16 18:48:47 +0900

    Rough domain implementation

--- src/main/java/com/github/arknote02/authentication/domain/port/AuthenticateServiceProxy.java
+++ src/main/java/com/github/arknote02/authentication/domain/port/AuthenticateServiceProxy.java
@@ -1,9 +1,9 @@
-package com.github.arknote02.authentication.domain.event;
+package com.github.arknote02.authentication.domain.port;
 
 import com.github.arknote02.authentication.domain.model.AccessToken;
 import com.github.arknote02.authentication.domain.model.Credential;
 
-public interface Authenticate {
+public interface AuthenticateServiceProxy {
 
   AccessToken authenticate(Credential credential);
 }
