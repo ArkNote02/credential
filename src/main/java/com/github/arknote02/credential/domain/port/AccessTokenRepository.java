3077f5c005f8430e7d11c1b2dbbd80fce932eff9
Queue <kentaro.kiuchi@outlook.jp>
2022-04-17 00:46:28 +0900

    Rough domain implementation

--- src/main/java/com/github/arknote02/credential/domain/port/AccessTokenRepository.java
+++ src/main/java/com/github/arknote02/credential/domain/port/AccessTokenRepository.java
@@ -3,7 +3,9 @@ package com.github.arknote02.credential.domain.port;
 import com.github.arknote02.credential.domain.model.AccessToken;
 import com.github.arknote02.credential.domain.model.LoginId;
 
-public interface AccessTokenNotifier {
+public interface AccessTokenRepository {
 
-  void notice(LoginId loginId, AccessToken accessToken);
+  LoginId find(AccessToken accessToken);
+
+  void store(LoginId loginId, AccessToken accessToken);
 }
--- src/main/java/com/github/arknote02/credential/domain/port/CredentialServiceProxy.java
+++ src/main/java/com/github/arknote02/credential/domain/port/CredentialServiceProxy.java
@@ -2,8 +2,11 @@ package com.github.arknote02.credential.domain.port;
 
 import com.github.arknote02.credential.domain.model.AccessToken;
 import com.github.arknote02.credential.domain.model.Credential;
+import com.github.arknote02.credential.domain.model.LoginId;
 
 public interface CredentialServiceProxy {
 
   AccessToken authenticate(Credential credential);
+
+  LoginId authenticate(AccessToken accessToken);
 }
